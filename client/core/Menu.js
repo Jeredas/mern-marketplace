a06bedabacc1e13dfece50c9440c4d7fa3ea8bbe
Uladzimir Kamarou <u.kamarou@softteco.com>
2022-02-07 17:23:25 +0300

    init: FinEng

--- client/core/Menu.js
+++ client/core/Menu.js
@@ -27,7 +27,7 @@ const Menu = withRouter(({history}) => (
   <AppBar position="static">
     <Toolbar>
       <Typography variant="h6" color="inherit">
-        MERN Marketplace
+      Financial Engeneering Lab
       </Typography>
       <div>
         <Link to="/">
--- config/config.js
+++ config/config.js
@@ -7,9 +7,9 @@ const config = {
     'mongodb://' + (process.env.IP || 'localhost') + ':' +
     (process.env.MONGO_PORT || '27017') +
     '/mernproject',
-  stripe_connect_test_client_id: 'YOUR_stripe_connect_test_client',
-  stripe_test_secret_key: 'YOUR_stripe_test_secret_key',
-  stripe_test_api_key: 'YOUR_stripe_test_api_key' 
+  stripe_connect_test_client_id: 'ca_L6kJmeY8o5HwC9TEtZG1oImB0oaGWQlE',
+  stripe_test_secret_key: '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',
+  stripe_test_api_key: 'pk_live_51KQWgUDdC7e3IDo5QEflIl5VL77qkRb5Ped0BOL7URyr8cHURWraD5qRkgxp6Re7vKM5Nm3vlmjbeUt093B2KVWg00Fj7IdRQk' 
 }
 
 export default config
