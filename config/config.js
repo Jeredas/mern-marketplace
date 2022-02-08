d51b992c63b742d8e22802f18f2937e47fcd28d8
Uladzimir Kamarou <u.kamarou@softteco.com>
2022-02-08 09:58:49 +0300

    fix: remove secret keys

--- config/config.js
+++ config/config.js
@@ -8,8 +8,8 @@ const config = {
     (process.env.MONGO_PORT || '27017') +
     '/mernproject',
   stripe_connect_test_client_id: 'ca_L6kJmeY8o5HwC9TEtZG1oImB0oaGWQlE',
-  stripe_test_secret_key: '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',
-  stripe_test_api_key: 'pk_live_51KQWgUDdC7e3IDo5QEflIl5VL77qkRb5Ped0BOL7URyr8cHURWraD5qRkgxp6Re7vKM5Nm3vlmjbeUt093B2KVWg00Fj7IdRQk' 
+  stripe_test_secret_key: 'your_secret_key',
+  stripe_test_api_key: 'api_secret_key' 
 }
 
 export default config
