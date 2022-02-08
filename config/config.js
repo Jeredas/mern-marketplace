0d0960687359d33fdf03b190a215aa573c227d8a
Uladzimir Kamarou <u.kamarou@softteco.com>
2022-02-08 14:19:34 +0300

    fix: switch ports

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 const config = {
   env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 5557,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri: process.env.MONGODB_URI ||
     process.env.MONGO_HOST ||
     'mongodb://' + (process.env.IP || 'localhost') + ':' +
-    (process.env.MONGO_PORT || '27017') +
+    (process.env.MONGO_PORT || '27000') +
     '/mernproject',
   stripe_connect_test_client_id: 'ca_L6kJmeY8o5HwC9TEtZG1oImB0oaGWQlE',
   stripe_test_secret_key: 'your_secret_key',
