ebf3d8cbbf6af93910ca6a0ba5cbe3a9e10aff77
Uladzimir Kamarou <u.kamarou@softteco.com>
2022-02-09 09:42:52 +0300

    Revert "fix: mongo port"
    This reverts commit 57dd9626bd357e1468e0a4bac7eeac3b3899e8e7.

--- config/config.js
+++ config/config.js
@@ -4,8 +4,8 @@ const config = {
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri: process.env.MONGODB_URI ||
     process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || '82.196.14.188') + ':' +
-    (process.env.MONGO_PORT || '27000') +
+    'mongodb://' + (process.env.IP || 'localhost') + ':' +
+    (process.env.MONGO_PORT || '27017') +
     '/mernproject',
   stripe_connect_test_client_id: 'YOUR_stripe_connect_test_client',
   stripe_test_secret_key: 'YOUR_stripe_test_secret_key',
